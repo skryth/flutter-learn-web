cc6f73e886656b00ce568c79887962b86e3671c4
Dn_Sh <shipilovdanil0@gmail.com>
2025-09-23 17:03:21 +0500

    fix: styles for CircleButtonBack and AuthForm

--- src/components/ListModules/index.tsx
+++ src/components/ListModules/index.tsx
@@ -1,19 +1,20 @@
 import { useMemo } from 'react';
 import { useAppSelector } from '../../app/store/hooks';
 import ModuleCard from '../ModuleCard'
-import styles from './index.module.css'
 import ModuleCardInDevelopment from '../ModuleCard/ModuleCardInDevelopment';
+import styles from './index.module.css'
 
 const ListModules = () => {
     const modules = useAppSelector(state => state.modules.list);
     const sortedModules = useMemo(() => 
         [...modules].sort((a, b) => a.order_index - b.order_index)
     , [modules]);
-
+    
   return (
     <div className={styles.list}>
-        {sortedModules.map(module => 
-            <>{module.lessons.length > 0 ?
+        {sortedModules.map(module => {
+            if (module.lessons.length > 0) {
+                return (
                 <ModuleCard 
                     key={module.id}
                     id={module.id}
@@ -21,14 +22,17 @@ const ListModules = () => {
                     order_index={module.order_index}
                     lessons={module.lessons}
                 />
-            :
-                <ModuleCardInDevelopment
-                    key={module.id}
-                    title={module.title}
-                    order_index={module.order_index} 
-                />
-            }</>
-        )}
+                )
+            } else {
+                return (
+                    <ModuleCardInDevelopment
+                        key={module.id}
+                        title={module.title}
+                        order_index={module.order_index} 
+                    />
+                )
+            }           
+        })}
     </div>
   )
 }
